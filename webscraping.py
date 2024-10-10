0425d99e2dfec96685ae3392dc2273d2fad142e0
Shaiane Boesing <shaianeboesingrb@gmail.com>
2024-10-09 21:43:25 -0300

    include more data

--- webscraping.py
+++ webscraping.py
@@ -36,7 +36,7 @@ if response.status_code == 200:
         print('Ano: ', year)
         print('Duração: ', duration)
         print('Classificação etária: ', age_rating)
-        print('Classificação: ', rating)
+        print('Avaliação: ', rating)
 
         # Requisição para a página do filme para obter mais detalhes
         movie_link = "https://www.imdb.com" + movie_path
@@ -46,8 +46,22 @@ if response.status_code == 200:
             imdb_movie_soup = BeautifulSoup(movie_response.text, 'html.parser')
 
             # Extraindo mais dados da página do filme
-            h1_element = imdb_movie_soup.find('h1', {'data-testid': 'hero__pageTitle'})
-            print(h1_element)
+            gender_list = imdb_movie_soup.find('div', class_="ipc-chip-list__scroller")
+            gender_list_name = []
+            for gender in gender_list.find_all('a'):
+                gender_name = gender.get_text()
+                gender_list_name.append(gender_name)
+            print('Gêneros: ', gender_list_name)
+            
+            actors_list = imdb_movie_soup.find_all('a', {"data-testid": "title-cast-item__actor"})
+            actors_list_name = []
+            characters_list = imdb_movie_soup.find_all('a', {"data-testid": "cast-item-characters-link"})
+            for actor, characters in zip(actors_list, characters_list):
+                # print(characters)
+                actor_character_name = f"{actor.get_text()} ({characters.span.get_text()})"
+                actors_list_name.append(actor_character_name)   
+            print("Atores: ", actors_list_name)
+            
         else:
             print(f'Erro ao acessar a página do filme: {movie_link}')
 else:
