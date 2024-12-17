caa5eea33e4fded4dc42dee1c15eaa2735b7ce66
Matheus Ed. <matheus.lafeta.moc@gmail.com>
2024-12-16 22:46:41 -0300

    remove dist

--- dashboard-api/dist/unified/unified.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { UnifiedService } from './unified.service';
-export declare class UnifiedController {
-    private readonly unifiedService;
-    constructor(unifiedService: UnifiedService);
-    getUnifiedData(): any;
-}
